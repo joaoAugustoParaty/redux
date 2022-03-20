069cbe7139cb80501782c745c8ca403eea66ef93
joaoAugustoParaty <joaodrow@gmail.com>
2022-03-20 17:59:27 -0300

    Reducers

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { SignUp } from './pages/SignUp';
+import { ShowData } from './pages/ShowData';
+import {ContextProvider} from './contexts/Context';
+
+const App = () =>{
+  return(
+    <ContextProvider>
+    <BrowserRouter>
+    <h1>Título da página</h1>
+    <hr/>
+    <Routes> 
+     <Route path="/" element={<SignUp/>}/>
+     <Route path="/exibir" element={<ShowData/>}/>
+    </Routes>
+    </BrowserRouter>
+    </ContextProvider>
+  )
 }
 
 export default App;
